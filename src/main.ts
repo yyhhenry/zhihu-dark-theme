b027b6100e8d183b3d880e8779b7f2b7b01a3562
yyhhenry <yyhhenry@foxmail.com>
2025-08-31 19:14:01 +0800

    Refactor theme handling functions for clarity
    - Extracted theme type definition for better readability.
    - Improved function structure for getting current theme.
    - Simplified URL handling in main function.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,36 @@
-function getPreferredColorScheme(): "light" | "dark" {
+type ThemeType = "light" | "dark";
+function getPreferredColorScheme(): ThemeType {
   return window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light";
 }
 
-function main() {
-  const currentUrl = new URL(window.location.href);
-  const preferredTheme = getPreferredColorScheme();
+function getCurrentTheme(): ThemeType {
+  // <html lang="zh" data-hairline="true" class="itcauecng" data-theme="light" data-rh="data-theme">
+  const htmlElement = document.documentElement;
+  return htmlElement.getAttribute("data-theme") === "dark" ? "dark" : "light";
+}
+async function asyncSleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  const themeParam = currentUrl.searchParams.get("theme");
-  if (themeParam != preferredTheme) {
-    currentUrl.searchParams.set("theme", preferredTheme);
+async function main() {
+  const preferredTheme = getPreferredColorScheme();
+  const currentTheme = getCurrentTheme();
+
+  const url = new URL(window.location.href);
+
+  if (currentTheme != preferredTheme) {
+    url.searchParams.set("theme", preferredTheme);
     console.log(`Try to reload the page with ${preferredTheme} theme`);
-    window.location.replace(currentUrl.toString());
+    window.location.replace(url);
   } else {
     console.log(`The page is already in ${preferredTheme} theme`);
+    if (url.searchParams.has("theme")) {
+      await asyncSleep(1000);
+      url.searchParams.delete("theme");
+      window.history.replaceState({}, "", url);
+    }
   }
 }
 
