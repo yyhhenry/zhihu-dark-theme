c7db6049dec8648c27fcc1596b657dc1cc7792f2
yyhhenry <yyhhenry@foxmail.com>
2025-12-30 00:25:08 +0800

    refactor: simplify package.json import method
    Replaced dynamic import of package.json with a static import statement for improved readability and consistency in the build script.

--- build.ts
+++ build.ts
@@ -1,11 +1,10 @@
 import * as esbuild from "esbuild";
+import pkg from "./package.json" with { type: "json" };
 import {
   type ScriptMetadata,
   generateBanner,
   iconFromDomain,
 } from "./utils/metadata.ts";
-
-const pkg = await import("./package.json", { assert: { type: "json" } });
 
 const metadata: ScriptMetadata = {
   name: pkg.name,
